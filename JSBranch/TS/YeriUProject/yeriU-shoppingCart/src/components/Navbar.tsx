6c9fd60e4be356bd5b770bc17b8ec79a58281807
Adan Lopez <evantomoki@gmail.com>
2025-05-31 00:20:09 -0600

    links to different pages added on Navbar component

--- JSBranch/TS/YeriUProject/yeriU-shoppingCart/src/components/Navbar.tsx
+++ JSBranch/TS/YeriUProject/yeriU-shoppingCart/src/components/Navbar.tsx
@@ -1,10 +1,23 @@
-import { Container, Navbar as NavbarTsume } from "react-bootstrap"
+import { Container, Nav, Navbar as NavbarTsume } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
 import "./Navbar.css"
 
 export function Navbar () {
      return (
           <NavbarTsume className="bg-white shadow-sm mb-3">
-               <Container>Nav</Container>
+               <Container>
+                    <Nav className="me-auto">
+                         <Nav.Link to="/" as={NavLink}>
+                         Inicio
+                         </Nav.Link>
+                         <Nav.Link to="/services" as={NavLink}>
+                         Diseños
+                         </Nav.Link>
+                         <Nav.Link to="/designs" as={NavLink}>
+                         Servicios
+                         </Nav.Link>
+                    </Nav>
+               </Container>
           </NavbarTsume>
      )
 }
